ab0121b2f0af65e427652dbf61ee8690b15115b0
CJC0512 <ehdchs2006@gmail.com>
2024-02-22 14:25:39 +0900

    chap01 완

---
+++ chap01-mybatis-spring-lecture-source/src/main/java/com/ohgiraffers/mybatisspring/section01/factorybean/MenuDTO.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.mybatisspring.section01.factorybean;
+
+public class MenuDTO {
+
+    private int menuCode;
+    private String menuName;
+    private int menuPrice;
+    private int categoryCode;
+    private String categoryStatus;
+}
